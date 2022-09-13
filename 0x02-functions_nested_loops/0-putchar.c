896bbca5329eddb66cadb26eac4a48a2616157e6
Nascreatz <nascreatz16@gmail.com>
2022-09-13 13:37:24 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>                                                                                                 
+                                                                                                                   
+/**                                                                                                                
+ * main - main function
+ * Return: 0                                                                                                       
+ */                                                                                                                
+int main (void)                                                                                                    
+{
+	int n = "_putchar";
+
+	printf("%d\n", n);
+	return (0);
+}
