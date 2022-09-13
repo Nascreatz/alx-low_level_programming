354ae9ff896b69bef8907a8ef70ef8835f7dc5f4
Nascreatz <nascreatz16@gmail.com>
2022-09-12 20:48:05 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,12 @@
  * Description:printinggg single digits numbers of base 10 starting from 01
  * Return: 0
  */
-
 int main(void)
 {
 	int x;
 	
 	for (x = 48; x <= 57; x++)
-	{
+	}
 		putchar(x);
 	}
 	putchar('\n');
