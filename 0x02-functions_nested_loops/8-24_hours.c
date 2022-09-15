1e46b52c90390a600d4a01e3e53260d64cb5b883
Nascreatz <nascreatz16@gmail.com>
2022-09-14 17:15:51 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,6 @@
 
 /**
  * jack_bauer - Day timing
- * Return: always 0
  */
 void jack_bauer(void)
 {
@@ -10,12 +9,14 @@ void jack_bauer(void)
 
 	for (h = 0; h <= 23; h++)
 	{
-		_putchar((h / 10) + '0');
-		_putchar((h % 10) + '0');
-		_putchar(':');
-		_putchar((s / 10) + '0');
-		_putchar((s % 10) + '0');
-		_putchar('\n');
-		return (0);
+		for (s = 0; s <= 59; s++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((s / 10) + '0');
+			_putchar((s % 10) + '0');
+			\_putchar('\n');
+		}
 	}
 }
