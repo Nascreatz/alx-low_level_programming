5dce390946a4e92308c0de621e43a18288a69178
Nascreat <nascreatz16@gmail.com>
2022-10-24 20:16:41 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,47 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: double pointer to head of list
- * @idx: index to add new node
- * @n: value at new node
- * Return: address of new node, or NULL if failed
+ * insert_nodeint_at_index - function with 3 arguments
+ * @head: pointer to head pointer of first node in linked list
+ * @idx: index of list
+ * @n: value integer
+ *
+ * Description: inserts a new node at a given position
+ * Return: address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
-	listint_t *newNode;
+	listint_t *temp, *cursor;
+	unsigned int count = 0;
 
-	register uint i;
-
-	newNode = malloc(sizeof(listint_t));
-	if (!head || !newNode)
+	if (head == NULL)
 		return (NULL);
-	newNode->n = n;
-	if (!idx)
-	{
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
-	current = *head;
-	while (current)
-	{
-		if (i == idx - 1)
-		{
-			newNode->next = current->next;
-			current->next = newNode;
-			return (newNode);
-		}
-		i++;
-		current = current->next;
-	}
-	free(newNode);
-	return (NULL);
-}
 
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	cursor = *head;
+
+	if (idx == 0)
+	{
+		temp->next = *head;
+		*head = temp;
+		return (*head);
+	}
+
+	while (cursor != NULL)
+	{
+		if (count == idx - 1)
+		{
+			temp->next = cursor->next;
+			cursor->next = temp;
+		}
+		count++;
+		cursor = cursor->next;
+	}
+	if (idx > count)
+		return (NULL);
+	return (temp);
+
+}
